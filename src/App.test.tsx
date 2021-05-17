7b978a204b1e8d63c953357c8798d1940c8a22a8
Boris <rozumniy.boris.work@gmail.com>
2021-05-17 13:12:07 +0300

    corrected tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
-import { CostInput } from './CostInput';
 
 it('renders learn react link', () => {
   render(<App />);
@@ -9,60 +8,22 @@ it('renders learn react link', () => {
   expect(title).toBeInTheDocument();
   const task = screen.queryByText(/add task/i);
   expect(task).toBeInTheDocument();
-  expect(screen.queryByText('*')).toBeNull();
   expect(screen.getByPlaceholderText(/enter the task name/i)).toBeInTheDocument();
   expect(screen.getAllByRole('textbox')[0]).toHaveAttribute('name', 'new task name');
-  expect(screen.getByRole('list')).toBeInTheDocument();
+});
+
+it('adding new tasks', () => {
+  render(<App />);
   expect(screen.getByRole('list').childElementCount).toBe(0);
-});
-
-it('renders the task item', () => {
-  render(<App />);
+  const input = screen.getByPlaceholderText("Enter the task name");
+  fireEvent.change(input, { target: { value: 'task 1' } })
+  expect(input.value).toBe('task 1')
   fireEvent.click(screen.getByText(/add task/i));
-  expect(screen.queryByText('*')).toBeInTheDocument();
-});
-
-it('adding new task', () => {
-  render(<App />);
-  fireEvent.click(screen.getByText(/add task/i));
-  expect(screen.queryByText('*')).toBeInTheDocument();
-  // expect(screen.getByRole('list').children[0]).toBe(1);
   expect(screen.getByRole('list').childElementCount).toBe(1);
+  expect(screen.getByRole('list').children[0].textContent).toBe('task 1');
+  
+  fireEvent.change(input, { target: { value: 'task 2' } })
+  fireEvent.click(screen.getByText(/add task/i));
+  expect(screen.getByRole('list').childElementCount).toBe(2);
+  expect(screen.getByRole('list').children[1].textContent).toBe('task 2');
 });
-
-const setup = () => {
-  const utils = render(<CostInput />)
-  const input = utils.getByLabelText('cost-input')
-  return {
-    input,
-    ...utils,
-  }
-};
-
-test('It should keep a $ in front of the input', () => {
-  const { input } = setup()
-  fireEvent.change(input, { target: { value: '23' } })
-  console.log(input)
-  expect(input.value).toBe('$23')
-});
-
-test('It should allow a $ to be in the input when the value is changed', () => {
-  const { input } = setup()
-  fireEvent.change(input, { target: { value: '$23.0' } })
-  expect(input._valueTracker.getValue()).toBe('$23.0')
-});
-
-test('It should not allow letters to be inputted', () => {
-  const { input } = setup()
-  expect(input.value).toBe('') // empty before
-  fireEvent.change(input, { target: { value: 'Good Day' } })
-  expect(input._valueTracker.getValue()).toBe('') //empty after
-});
-
-xit('It should allow the $ to be deleted', () => {
-  const { input } = setup()
-  fireEvent.change(input, { target: { value: '23' } })
-  expect(input.value).toBe('$23') // need to make a change so React registers "" as a change
-  fireEvent.change(input, { target: { value: '' } })
-  expect(input._valueTracker.getValue()).toBe('')
-})
