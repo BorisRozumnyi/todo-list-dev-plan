89d91175a29cd202beff0cdc6c3b4e83dca2010a
Boris <rozumniy.boris.work@gmail.com>
2021-05-20 22:51:40 +0300

    Revert "CostInput, TaskList"
    This reverts commit 6c925658d5c5690de1ad9f0ff6c9af1901fbdb9d.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,68 +1,9 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
-import { CostInput } from './CostInput';
 
-it('renders learn react link', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const title = screen.getByText(/todo list/i);
-  expect(title).toBeInTheDocument();
-  const task = screen.queryByText(/add task/i);
-  expect(task).toBeInTheDocument();
-  expect(screen.queryByText('*')).toBeNull();
-  expect(screen.getByPlaceholderText(/enter the task name/i)).toBeInTheDocument();
-  expect(screen.getAllByRole('textbox')[0]).toHaveAttribute('name', 'new task name');
-  expect(screen.getByRole('list')).toBeInTheDocument();
-  expect(screen.getByRole('list').childElementCount).toBe(0);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
-
-it('renders the task item', () => {
-  render(<App />);
-  fireEvent.click(screen.getByText(/add task/i));
-  expect(screen.queryByText('*')).toBeInTheDocument();
-});
-
-it('adding new task', () => {
-  render(<App />);
-  fireEvent.click(screen.getByText(/add task/i));
-  expect(screen.queryByText('*')).toBeInTheDocument();
-  // expect(screen.getByRole('list').children[0]).toBe(1);
-  expect(screen.getByRole('list').childElementCount).toBe(1);
-});
-
-const setup = () => {
-  const utils = render(<CostInput />)
-  const input = utils.getByLabelText('cost-input')
-  return {
-    input,
-    ...utils,
-  }
-};
-
-test('It should keep a $ in front of the input', () => {
-  const { input } = setup()
-  fireEvent.change(input, { target: { value: '23' } })
-  console.log(input)
-  expect(input.value).toBe('$23')
-});
-
-test('It should allow a $ to be in the input when the value is changed', () => {
-  const { input } = setup()
-  fireEvent.change(input, { target: { value: '$23.0' } })
-  expect(input._valueTracker.getValue()).toBe('$23.0')
-});
-
-test('It should not allow letters to be inputted', () => {
-  const { input } = setup()
-  expect(input.value).toBe('') // empty before
-  fireEvent.change(input, { target: { value: 'Good Day' } })
-  expect(input._valueTracker.getValue()).toBe('') //empty after
-});
-
-xit('It should allow the $ to be deleted', () => {
-  const { input } = setup()
-  fireEvent.change(input, { target: { value: '23' } })
-  expect(input.value).toBe('$23') // need to make a change so React registers "" as a change
-  fireEvent.change(input, { target: { value: '' } })
-  expect(input._valueTracker.getValue()).toBe('')
-})
--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,26 @@
-import React, { FormEvent } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { CostInput } from './CostInput';
-import { TaskList } from './TaskList';
 
-export type TTask = {
-  title: string; isCompleted: boolean, id: number
-};
-
-type MyState = { isOpen: boolean, taskList: TTask[], newTaskValue: string };
-
-class App extends React.Component<{}, MyState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      isOpen: false,
-      taskList: [],
-      newTaskValue: '',
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  };
-
-  handleClick() {
-    const { newTaskValue } = this.state;
-    this.setState({ isOpen: !this.state.isOpen })
-    const newTask = {
-      title: newTaskValue, isCompleted: false, id: Date.now(),
-    };
-    // this.state.taskList.push(newTask)
-    const tasks = [...this.state.taskList, newTask];
-    this.setState({ taskList: tasks })
-  };
-
-  handleChange(e: React.FormEvent<HTMLInputElement>) {
-    const { value } = e.currentTarget;
-    this.setState({ newTaskValue: value });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Todo List</h1>
-        <input type="text" name="new task name" placeholder="Enter the task name" onChange={this.handleChange} />
-        <CostInput />
-        <button onClick={this.handleClick}>Add task</button>
-        {this.state.isOpen && <span>*</span>}
-        <TaskList tasks={this.state.taskList} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
